d70b680fc15bfc5c33dfb883ca09ab0c38e2d9d4
VaraPrasad63020 <141317413+VaraPrasad63020@users.noreply.github.com>
2023-09-20 19:03:52 +0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws"{
+    region="ap-south-1"
+    access_key="AKIAZNJHT2M6IDKJSVDC"
+    secret_key="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"
+
+}
+resource "aws_instance" "terraform"{
+    ami="ami-0c3c912253ec8a579"
+    instance_type="t2_micro"
+}
